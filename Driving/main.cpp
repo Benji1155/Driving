13b84e34bc744f629655ad0dce29db49e3652514
Maxwell Benjamin Presley <Maxwell.Presley@mds.ac.nz>
2022-03-12 16:32:57 +1300

    stinky

--- Driving/main.cpp
+++ Driving/main.cpp
@@ -18,22 +18,7 @@ int main()
 
 
 
-	//asking for 2 numbers 
-
-	cout << "Enter a whole number: ";
-
-	cin >> n1;
+	/
 
 
-
-	//doing maths 
-
-	if (n1 % 2 == 0)
-
-		cout << n1 << " is even.";
-
-	else
-
-		cout << n1 << " is odd.";
-
-	return(0);
+		stinky
